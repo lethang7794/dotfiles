2288109e4bb23fb4f7657e78c2a4c5fa37098b9b
Le Quang Thang <lethang7794@gmail.com>
2024-07-26 02:33:32 +0700

    feat(nvim): config whichkeys, cheatsheat

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -5,7 +5,7 @@ return {
     config = function()
       require "configs.conform"
     end
-  }, 
+  },
   { "junegunn/goyo.vim" },
   {
     "neovim/nvim-lspconfig",
@@ -15,15 +15,15 @@ return {
     end,
   },
   {
-  	"williamboman/mason.nvim",
-  	opts = {
-  		ensure_installed = {
-  			"lua-language-server", "stylua",
-  			"html-lsp", "css-lsp" , "prettier"
-  		},
-  	},
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server", "stylua",
+        "html-lsp", "css-lsp", "prettier"
+      },
+    },
   },
-  
+
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
@@ -49,19 +49,55 @@ return {
       -- your configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
+      preset = "helix",
+      delay = function(ctx)
+        return ctx.plugin and 0 or 300
+      end,
+      win = {
+        padding = { 1, 2 },
+      },
+      keys = {
+        scroll_down = "<c-f>", -- binding to scroll down inside the popup
+        scroll_up = "<c-b>",   -- binding to scroll up inside the popup
+      },
       icons = {
         separator = " ",
+        breadcrumb = "",
         keys = {
+          Up = " ",
+          Down = " ",
+          Left = " ",
+          Right = " ",
           C = "C-",
           M = "A-",
+          D = "󰘳 ",
           S = "S-",
-          Tab = "Tab ",
+          CR = "󰌑 ",
+          Esc = "󱊷 ",
+          ScrollWheelDown = "󱕐 ",
+          ScrollWheelUp = "󱕑 ",
+          NL = "󰌑 ",
+          BS = "󰁮",
+          Space = "󱁐",
+          Tab = "Tab",
+          F1 = "󱊫",
+          F2 = "󱊬",
+          F3 = "󱊭",
+          F4 = "󱊮",
+          F5 = "󱊯",
+          F6 = "󱊰",
+          F7 = "󱊱",
+          F8 = "󱊲",
+          F9 = "󱊳",
+          F10 = "󱊴",
+          F11 = "󱊵",
+          F12 = "󱊶",
         }
       }
     },
     keys = {
       {
-        "<leader>?",
+        "<leader><F1>",
         function()
           require("which-key").show({ global = false })
         end,
@@ -69,5 +105,41 @@ return {
       },
     },
   },
-  { 'echasnovski/mini.icons', version = false }
+  { 'echasnovski/mini.icons', version = false },
+  {
+    "doctorfree/cheatsheet.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      { "nvim-telescope/telescope.nvim" },
+      { "nvim-lua/popup.nvim" },
+      { "nvim-lua/plenary.nvim" },
+    },
+    config = function()
+      local ctactions = require("cheatsheet.telescope.actions")
+      require("cheatsheet").setup({
+        bundled_cheetsheets = {
+          enabled = { "default", "lua", "markdown", "regex", "netrw", "unicode" },
+          disabled = { "nerd-fonts" },
+        },
+        bundled_plugin_cheatsheets = {
+          enabled = {
+            "auto-session",
+            "goto-preview",
+            "octo.nvim",
+            "telescope.nvim",
+            "vim-easy-align",
+            "vim-sandwich",
+          },
+          disabled = { "gitsigns" },
+        },
+        include_only_installed_plugins = true,
+        telescope_mappings = {
+          ["<CR>"] = ctactions.select_or_fill_commandline,
+          ["<A-CR>"] = ctactions.select_or_execute,
+          ["<C-Y>"] = ctactions.copy_cheat_value,
+          ["<C-E>"] = ctactions.edit_user_cheatsheet,
+        },
+      })
+    end,
+  }
 }
