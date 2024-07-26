6c13dde2deb51a8a3fb693086b32f234db8a53f5
Le Quang Thang <lethang7794@gmail.com>
2024-07-26 17:04:10 +0700

    feat(nvim): disable mousescroll

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -3,6 +3,8 @@ require "nvchad.options"
 -- add yours here!
 
 local o = vim.o
+
 o.number = true
 o.relativenumber = true
-o.cursorlineopt ='both' -- to enable cursorline!
+o.cursorlineopt = 'both'      -- Enable cursorline
+o.mousescroll = "ver:0,hor:0" -- Disalble mousescroll
