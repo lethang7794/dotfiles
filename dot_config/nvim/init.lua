eb800d6568046038fba5ad166af1e2f0df84379c
Le Quang Thang <lethang7794@gmail.com>
2024-08-13 15:48:00 +0700

    feat(nvim): markdown fenced languages

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,6 +1,7 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 vim.g.mapleader = " "
 
+vim.g.markdown_fenced_languages = { "go", "typescript", "javascript", "bash", "html", "css", "yaml", "json", "terraform" }
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
