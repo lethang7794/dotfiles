707f8596ad644f9934a6250f3f18eb7a900261ae
Le Quang Thang <lethang7794@gmail.com>
2024-09-20 22:38:06 +0700

    feat(nvim): markmap

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -45,5 +45,19 @@ return {
   { "sindrets/diffview.nvim",              event = "VeryLazy", },
   { "preservim/vim-pencil",                event = "VeryLazy", },
   { "lukas-reineke/indent-blankline.nvim", event = "VeryLazy", main = "ibl", opts = {} },
-  { "HiPhish/rainbow-delimiters.nvim",     event = "VeryLazy" }
+  { "HiPhish/rainbow-delimiters.nvim",     event = "VeryLazy" },
+
+  --  [markdown markmap]
+  --  https://github.com/Zeioth/markmap.nvim
+  {
+    "Zeioth/markmap.nvim",
+    build = "yarn global add markmap-cli",
+    cmd = { "MarkmapOpen", "MarkmapSave", "MarkmapWatch", "MarkmapWatchStop" },
+    opts = {
+      html_output = "/tmp/markmap.html", -- (default) Setting a empty string "" here means: [Current buffer path].html
+      hide_toolbar = false, -- (default)
+      grace_period = 3600000 -- (default) Stops markmap watch after 60 minutes. Set it to 0 to disable the grace_period.
+    },
+    config = function(_, opts) require("markmap").setup(opts) end
+  },
 }
