180d242e3529643ce1ec1bd09de2ae6b7e9e1485
Le Quang Thang <lethang7794@gmail.com>
2024-10-29 17:38:27 +0700

    nvim: sync config

--- dot_config/nvim/lua/configs/conform.lua
+++ dot_config/nvim/lua/configs/conform.lua
@@ -1,15 +1,26 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    html = { "prettierd", "prettier", stop_after_first = true },
+    css = { "prettierd", "prettier", stop_after_first = true },
+    javascript = { "prettierd", "prettier", stop_after_first = true },
+    markdown = { "prettierd", "prettier", stop_after_first = true },
+    ["*"] = { "codespell" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  default_format_opts = {
+    lsp_format = "fallback",
+  },
+
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+  -- Conform will notify you when a formatter errors
+  notify_on_error = true,
+  -- Conform will notify you when no formatters are available for the buffer
+  notify_no_formatters = true,
 }
 
-return options
+require("conform").setup(options)
--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -2,5 +2,9 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+
+o.number = true               -- Show line number
+o.relativenumber = true       -- Show relative line number
+o.cursorlineopt = 'both'      -- Enable cursorline
+o.mousescroll = "ver:0,hor:0" -- Disalble mousescroll
