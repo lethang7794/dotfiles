f538450112eb11294ab0ed8c0ace2456b895e9cb
Le Quang Thang <lethang7794@gmail.com>
2024-11-01 00:55:52 +0700

    zsh: magic-enter

---
+++ .config/zsh/conf.d/magic-enter.zsh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+# magic-enter: Configure magic-enter.
+#
+# Makes your enter key magical, by binding commonly used commands to it.
+# https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/magic-enter
+
+MAGIC_ENTER_GIT_COMMAND='git status -su .'
+MAGIC_ENTER_OTHER_COMMAND='eza --icons'
